abc6b9ec2f5f012d40060fecc123f98c733643ff
Axel Froborg <axel.froborg@gmail.com>
2020-11-03 14:36:34 +0100

    shorten calculations

--- pt-workspace/Lab01-introduktion/src/Calculator.java
+++ pt-workspace/Lab01-introduktion/src/Calculator.java
@@ -7,27 +7,11 @@ public class Calculator {
 		double nbr1 = scan.nextDouble();
 		double nbr2 = scan.nextDouble();
 
-		System.out.println("Summan av talen är " + sum(nbr1, nbr2));
-		System.out.println("Skillnaden mellan talen är " + difference(nbr1, nbr2));
-		System.out.println("Produkten av talen är " + product(nbr1, nbr2));
-		System.out.println("Kvoten mellan talen är " + quotient(nbr1, nbr2));
+		System.out.println("Summan av talen är " + (nbr1 + nbr2));
+		System.out.println("Skillnaden mellan talen är " + (nbr1 - nbr2));
+		System.out.println("Produkten av talen är " + (nbr1 * nbr2));
+		System.out.println("Kvoten mellan talen är " + (nbr1 / nbr2));
 
 		scan.close();
-	}
-
-	private static double sum(double nbr1, double nbr2) {
-		return nbr1 + nbr2;
-	}
-
-	private static double difference(double nbr1, double nbr2) {
-		return nbr1 - nbr2;
-	}
-
-	private static double product(double nbr1, double nbr2) {
-		return nbr1 * nbr2;
-	}
-
-	private static double quotient(double nbr1, double nbr2) {
-		return nbr1 / nbr2;
 	}
 }
